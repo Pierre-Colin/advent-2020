027d9ba32bf70f73208a1947a5348e8f53bae497
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2021-02-05 23:45:37 +0100

    Optimize code for day 15
    	The only further optimization I can think of would be to add
    	the `inline` qualifier which in this case would do nothing. Day
    	15 may very well be the hardest day computationally speaking.

--- 15.c
+++ 15.c
@@ -17,91 +17,56 @@
 	else \
 		fputs(s "\n", stderr);
 
-typedef struct {
-	uintmax_t last, second;
-} History;
-
-static History *num = NULL;
-static size_t sz = 0;
-
-static bool
-speak(const size_t n, const uintmax_t turn)
+static uint_fast32_t
+playturn(uint_fast32_t num[UINT32_C(30000000)],
+         const uint_fast32_t turn,
+         const uint_fast32_t last)
 {
-	if (sz <= n) {
-		History *const new = realloc(num, (n + 1) * sizeof(History));
-		if (new == NULL)
-			return false;
-		for (size_t i = sz; i <= n; i++)
-			new[i].last = new[i].second = UINTMAX_MAX;
-		num = new;
-		sz = n + 1;
-	}
-	num[n].second = num[n].last;
-	num[n].last = turn;
-	return true;
-}
-
-static size_t
-playturn(const uintmax_t turn, const size_t last)
-{
-	if (num[last].second == UINTMAX_MAX) {
-		speak(0, turn);
-		return 0;
-	} else if (num[last].last - num[last].second > SIZE_MAX) {
-		fprintf(stderr,
-		        "Cannot store %ju without wraparound\n",
-		        num[last].last - num[last].second);
-		exit(EXIT_FAILURE);
-	} else {
-		const size_t n = num[last].last - num[last].second;
-		if (!speak(n, turn)) {
-			PRINT_ERR("Could not speak the next number");
-			exit(EXIT_FAILURE);
-		}
-		return n;
-	}
-}
-
-static void
-freenum(void)
-{
-	free(num);
+	const uint_fast32_t temp = num[last];
+	num[last] = turn;
+	return temp > turn? 0 : turn - temp;
 }
 
 int
 day15(FILE * const in)
 {
-	if (atexit(freenum) != 0)
-		fputs("Call to `atexit` failed; memory may leak\n", stderr);
-	uintmax_t turn = 0;
-	size_t input, last = SIZE_MAX;
-	int scanres;
+	uint_fast32_t turn = 0, last = UINT_FAST32_MAX, input;
 	errno = 0;
-	while ((scanres = fscanf(in, "%zu", &input)) == 1) {
-		if (!speak(input, turn++)) {
-			PRINT_ERR("Could not speak a starting number");
-			return EXIT_FAILURE;
-		}
+	uint_fast32_t *num = malloc(UINT32_C(30000000) * sizeof(uint_fast32_t));
+	if (num == NULL) {
+		PRINT_ERR("Could not allocate the number history");
+		return EXIT_FAILURE;
+	}
+	for (uint_fast32_t i = 0; i < UINT32_C(30000000); i++)
+		num[i] = UINT_FAST32_MAX;
+	while (fscanf(in, "%" SCNuFAST32, &input) == 1) {
+		if (last != UINT_FAST32_MAX)
+			num[last] = turn;
+		last = input;
 		const int next = fgetc(in);
 		if (next != ',' && next != '\n' && next != EOF) {
 			fprintf(stderr, "Unexpected character: %c\n", next);
+			free(num);
 			return EXIT_FAILURE;
 		}
-		last = input;
+		turn++;
 	}
 	if (!feof(in)) {
 		PRINT_ERR("Error occured during puzzle input parsing");
+		free(num);
 		return EXIT_FAILURE;
 	}
-	if (last > sz) {
+	if (last > UINT32_C(30000000)) {
 		fputs("Number list was empty\n", stderr);
+		free(num);
 		return EXIT_FAILURE;
 	}
-	while (turn < UINTMAX_C(2020))
-		last = playturn(turn++, last);
-	printf("2020th\t%zu\n", last);
-	while (turn < UINTMAX_C(30000000))
-		last = playturn(turn++, last);
-	printf("30Mth\t%zu\n", last);
+	while (turn < UINT32_C(2020))
+		last = playturn(num, turn++, last);
+	printf("2020th\t%" PRIuFAST32 "\n", last);
+	while (turn < UINT32_C(30000000))
+		last = playturn(num, turn++, last);
+	printf("30Mth\t%" PRIuFAST32 "\n", last);
+	free(num);
 	return EXIT_SUCCESS;
 }
