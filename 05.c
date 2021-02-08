1bfd561935165a5db8178f6cd04e097562928443
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2021-02-08 20:03:23 +0100

    Adjust coding style for day 5

--- 05.c
+++ 05.c
@@ -12,14 +12,14 @@
 #include <stdlib.h>
 
 static void
-parseseatpart(uint16_t *const id,
-              const char *const input,
-              const uint8_t begin,
-              const uint8_t end,
+parseseatpart(uint_fast16_t * const restrict id,
+              const char * const restrict input,
+              const uint_fast8_t begin,
+              const uint_fast8_t end,
               const char high,
               const char low)
 {
-	for (uint8_t i = begin; i < end; i++) {
+	for (uint_fast8_t i = begin; i < end; i++) {
 		*id *= 2;
 		if (input[i] == high) {
 			(*id)++;
@@ -33,34 +33,35 @@ parseseatpart(uint16_t *const id,
 int
 day05(FILE * const in)
 {
-	uint16_t highest = 0;
-	uint8_t present[128] = { 0 };
+	uint_fast16_t highest = 0;
+	uint_fast8_t present[128] = { 0 };
 	char input[11];
-	int scanres;
-	while ((scanres = fscanf(in, "%10[FBLR]\n", input)) != EOF) {
-		if (scanres < 1) {
-			if (errno != 0)
-				perror("Failed to input boarding pass");
-			else
-				fputs("Bad input format\n", stderr);
-			return EXIT_FAILURE;
-		}
-		uint16_t id = 0;
+	errno = 0;
+	while (fscanf(in, "%10[FBLR]", input) == 1) {
+		uint_fast16_t id = 0;
 		parseseatpart(&id, input, 0, 7, 'B', 'F');
 		parseseatpart(&id, input, 7, 10, 'R', 'L');
 		if (id > highest)
 			highest = id;
-		const div_t d = div(id, 8);
-		present[d.quot] |= 1 << d.rem;
+		present[id / 8] |= 1 << (id % 8);
+		if (fgetc(in) != '\n')
+			break;
 	}
-	printf("Highest\t%" PRIu16 "\n", highest);
+	if (!feof(in) || ferror(in)) {
+		if (errno != 0)
+			perror("Failed to input boarding pass");
+		else
+			fputs("Bad input format\n", stderr);
+		return EXIT_FAILURE;
+	}
+	printf("Highest\t%" PRIuFAST16 "\n", highest);
 	bool began = false;
-	for (uint16_t i = 0; i < 128; i++) {
-		for (uint8_t j = 0; j < 8; j++) {
+	for (uint_fast16_t i = 0; i < 128; i++) {
+		for (uint_fast8_t j = 0; j < 8; j++) {
 			if (!began && (present[i] & (1 << j)))
 				began = true;
 			if (began && !(present[i] & (1 << j))) {
-				printf("Seat\t%" PRIu16 "\n", 8 * i + j);
+				printf("Seat\t%" PRIuFAST16 "\n", 8 * i + j);
 				return EXIT_SUCCESS;
 			}
 		}
