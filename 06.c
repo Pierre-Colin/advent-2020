00550cb542ded87ee5edaa5c20a1e5d38e8c2e5f
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2021-02-10 15:13:19 +0100

    Adjust coding style for day 6

--- 06.c
+++ 06.c
@@ -6,58 +6,82 @@
  * http://www.wtfpl.net/ for more details.
  */
 #include <errno.h>
-#include <inttypes.h>
+#include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DIGITS(T) (CHAR_BIT * 10 * sizeof(T) / (9 * sizeof(char)))
+
 static void
-updatetotals(const uint32_t *const anyone,
-             uint64_t *const atotal,
-             const uint32_t *const everyone,
-             uint64_t *const etotal)
+updatetotals(const uint_fast32_t acurrent,
+             uintmax_t * const restrict atotal,
+             const uint_fast32_t ecurrent,
+             uintmax_t * const restrict etotal)
 {
-	for (uint8_t i = 0; i < 26; i++) {
-		if (*anyone & (1u << i))
+	for (uint_fast8_t i = 0; i < 26; i++) {
+		if (acurrent & (1u << i))
 			(*atotal)++;
-		if (*everyone & (1u << i))
+		if (ecurrent & (1u << i))
 			(*etotal)++;
+	}
+}
+
+static void
+parseerr(const uintmax_t line)
+{
+	if (errno != 0) {
+		char buf[19 + DIGITS(uintmax_t)];
+		sprintf(buf, "Bad input on line %ju", line);
+		perror(buf);
+	} else {
+		fprintf(stderr, "Bad input on line %ju\n", line);
 	}
 }
 
 int
 day06(FILE * const in)
 {
-	uint64_t atotal = 0;
-	uint64_t etotal = 0;
-	uint32_t anyone = 0;
-	uint32_t everyone = 0x03ffffff;
-	char input[27];
-	int scanres;
-	while ((scanres = fscanf(in, "%26[a-z]%*1[\n]", input)) != EOF) {
-		if (scanres < 1) {
-			if (errno != 0) {
-				perror("Failed to parse input");
+	uintmax_t atotal = 0, etotal = 0, line = 1;
+	uint_fast32_t acurrent = 0, ecurrent = 0x03ffffff;
+	errno = 0;
+	while (!feof(in) && !ferror(in)) {
+		char input[27];
+		if (fscanf(in, "%26[a-z]", input) == 1) {
+			uint_fast32_t this = 0;
+			for (const char *c = input; *c != 0; c++)
+				this |= UINT32_C(1) << (*c - 'a');
+			acurrent |= this;
+			ecurrent = 0x03ffffff & (ecurrent & this);
+			const int end = fgetc(in);
+			if (end != EOF && end != '\n') {
+				parseerr(line);
 				return EXIT_FAILURE;
-			} else if (fgetc(in) == '\n') {
-				updatetotals(&anyone,
+			} else if (end == EOF) {
+				updatetotals(acurrent,
 				             &atotal,
-				             &everyone,
+				             ecurrent,
 				             &etotal);
-				anyone = 0;
-				everyone = 0x03ffffff;
-			} else {
-				fputs("Bad input format\n", stderr);
-				return EXIT_FAILURE;
 			}
 		} else {
-			uint32_t me = 0;
-			for (const char *c = input; *c != 0; c++)
-				me |= 1u << (*c - 'a');
-			anyone |= me;
-			everyone = 0x03ffffff & (everyone & me);
+			const int c = fgetc(in);
+			if (c != EOF && c != '\n') {
+				parseerr(line);
+				return EXIT_FAILURE;
+			}
+			updatetotals(acurrent, &atotal, ecurrent, &etotal);
+			acurrent = 0;
+			ecurrent = 0x03ffffff;
 		}
+		line++;
 	}
-	updatetotals(&anyone, &atotal, &everyone, &etotal);
-	printf("Any\t%" PRIu64 "\nEvery\t%" PRIu64 "\n", atotal, etotal);
+	if (!feof(in) || ferror(in)) {
+		if (errno != 0)
+			perror("Puzzle input parsing failed");
+		else
+			fputs("Puzzle input parsing failed\n", stderr);
+		return EXIT_FAILURE;
+	}
+	printf("Any\t%ju\nEvery\t%ju\n", atotal, etotal);
 	return EXIT_SUCCESS;
 }
