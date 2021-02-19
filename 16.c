c90b462aa88d862e790f745a36e6c86803867db6
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2021-02-19 13:20:51 +0100

    Adjust coding style for days 16 and 17
    	Day 17 was also optimized by ~10%

--- 16.c
+++ 16.c
@@ -6,23 +6,11 @@
  * http://www.wtfpl.net/ for more details.
  */
 #include <ctype.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <regex.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define PRINT_ERR(s) \
-	do { \
-		if (errno != 0) \
-			perror(s); \
-		else \
-			fputs(s "\n", stderr); \
-	} while (false);
-
-#define NUM_REG "(0|[1-9][0-9]*)"
 
 typedef enum {
 	CONSTRAINTS,
@@ -47,151 +35,127 @@ struct Node {
 typedef struct Node Node;
 
 static FieldDef *constr = NULL;
-static size_t cconstr = 0;
-static size_t nconstr = 0;
+static size_t cconstr = 0, nconstr = 0;
 static uintmax_t *yourticket = NULL;
 static Node *tickets = NULL;
-static regex_t defreg;
 
 static int
-getline(FILE * const in, char **input)
+getline(FILE * const restrict in, char ** const restrict input)
 {
-	errno = 0;
 	fscanf(in, "%*[ \t\n]");
 	return fscanf(in, "%m[^\n]", input);
 }
 
 static bool
-parsedef(const char *const input)
+parsedef(const char * const input)
 {
-	regmatch_t match[6];
-	if (regexec(&defreg, input, 6, match, 0) != 0) {
-		fprintf(stderr, "Bad input on line %s\n", input);
-		return false;
-	}
 	if (nconstr == cconstr) {
 		cconstr = (cconstr > 0)? 2 * cconstr : 1;
-		errno = 0;
 		FieldDef *temp = realloc(constr, cconstr * sizeof(FieldDef));
 		if (temp == NULL) {
-			PRINT_ERR("Allocation failed");
+			fputs("Could not allocate field definition\n", stderr);
 			return false;
 		}
 		constr = temp;
 	}
-	errno = 0;
-	char *const name = malloc((match[1].rm_eo + 1) * sizeof(char));
-	if (name == NULL) {
-		PRINT_ERR("Allocation failed");
+	constr[nconstr].name = NULL;
+	int n;
+	if (sscanf(input,
+	           "%m[A-Za-z ]: %ju-%ju or %ju-%ju%n",
+	           &constr[nconstr].name,
+	           &constr[nconstr].i[0].min,
+	           &constr[nconstr].i[0].max,
+	           &constr[nconstr].i[1].min,
+	           &constr[nconstr].i[1].max,
+	           &n) != 5
+	    || input[n] != 0) {
+		free(constr[nconstr].name);
 		return false;
 	}
-	strncpy(name, input, match[1].rm_eo);
-	name[match[1].rm_eo] = 0;
-	errno = 0;
-	int result = sscanf(input + match[2].rm_so,
-	                    "%" SCNuMAX "-%" SCNuMAX
-	                    " or %" SCNuMAX "-%" SCNuMAX,
-	                    &constr[nconstr].i[0].min,
-	                    &constr[nconstr].i[0].max,
-	                    &constr[nconstr].i[1].min,
-	                    &constr[nconstr].i[1].max);
-	if (result == EOF) {
-		fprintf(stderr,
-		        "Could not match intervals on line: %s\n",
-		        input);
-		free(name);
-		return false;
-	}
-	constr[nconstr++].name = name;
+	nconstr++;
 	return true;
 }
 
 static uintmax_t *
-parsefields(const char *const input)
+parsefields(const char * const input)
 {
-	errno = 0;
 	if (nconstr == 0)
 		return NULL;
-	uintmax_t *const fields = malloc(nconstr * sizeof(uintmax_t));
-	if (fields != NULL) {
-		int off;
-		const char *it = input;
-		for (size_t i = 0; i < nconstr; i++) {
-			errno = 0;
-			if (sscanf(it, "%" SCNuMAX "%n", fields + i, &off) < 1
-			    || (i != nconstr - 1 && it[off] != ',')) {
-				fprintf(stderr,
-				        "Could not match ticket at %zu: %s\n",
-					i,
-				        input);
-				free(fields); 
-				return NULL;
-			}
-			it += off + 1;
-		}
-		if (*(it - 1) != 0) {
+	uintmax_t * const fields = malloc(nconstr * sizeof(uintmax_t));
+	if (fields == NULL)
+		return NULL;
+	int off;
+	const char *it = input;
+	for (size_t i = 0; i < nconstr; i++) {
+		if (sscanf(it, "%ju%n", fields + i, &off) < 1
+		    || (i != nconstr - 1 && it[off] != ',')) {
 			fprintf(stderr,
-			        "Ticket is over %zu fields long: %s\n",
-			        nconstr,
-			        input);
-			free(fields);
+				"Could not match ticket at %zu: %s\n",
+				i,
+				input);
+			free(fields); 
 			return NULL;
 		}
+		it += off + 1;
+	}
+	if (*(it - 1) != 0) {
+		fprintf(stderr,
+			"Ticket is over %zu fields long: %s\n",
+			nconstr,
+			input);
+		free(fields);
+		return NULL;
 	}
 	return fields;
 }
 
 static bool
-parseyourticket(const char *const input)
+parseyourticket(const char * const input)
 {
 	if (yourticket == NULL) {
-		if ((yourticket = parsefields(input)) == NULL)
-			return false;
+		return (yourticket = parsefields(input)) != NULL;
 	} else {
 		fputs("Your ticket is ill-formed\n", stderr);
 		return false;
 	}
-	return true;
 }
 
 static bool
-isin(const uintmax_t val, const Interval i)
+isin(const uintmax_t val, const Interval * const i)
 {
-	return i.min <= val && val <= i.max;
+	return i->min <= val && val <= i->max;
 }
 
 static bool
-satisfies(const uintmax_t val, const FieldDef constr)
+satisfies(const uintmax_t val, const FieldDef * const constr)
 {
-	return isin(val, constr.i[0]) || isin(val, constr.i[1]);
+	return isin(val, &constr->i[0]) || isin(val, &constr->i[1]);
 }
 
 static bool
 isvalid(const uintmax_t val)
 {
 	for (size_t i = 0; i < nconstr; i++) {
-		if (satisfies(val, constr[i]))
+		if (satisfies(val, constr + i))
 			return true;
 	}
 	return false;
 }
 
 static bool
-parsenearbyticket(const char *input, Node **tail, uintmax_t *tser)
+parsenearbyticket(const char * const restrict input,
+                  Node ** const restrict tail,
+                  uintmax_t * const restrict tser)
 {
-	bool invalid = false;
-	uintmax_t *const fields = parsefields(input);
-	if (fields == NULL) {
-		const int temp = errno;
-		fprintf(stderr, "On nearby ticket: %s\n", input);
-		errno = temp;
+	uintmax_t * const fields = parsefields(input);
+	if (fields == NULL)
 		return false;
-	}
+	bool invalid = false;
 	for (size_t i = 0; i < nconstr; i++) {
 		if (!isvalid(fields[i])) {
 			invalid = true;
 			if (*tser > UINTMAX_MAX - fields[i]) {
-				fputs("Integer overflow detected\n", stderr);
+				fputs("Integer wraparound detected\n", stderr);
 				free(fields);
 				return false;
 			}
@@ -202,13 +166,9 @@ parsenearbyticket(const char *input, Node **tail, uintmax_t *tser)
 		free(fields);
 		return true;
 	}
-	errno = 0;
-	Node *node = malloc(sizeof(Node));
+	Node * const node = malloc(sizeof(Node));
 	if (node == NULL) {
 		free(fields);
-		const int temp = errno;
-		fprintf(stderr, "On nearby ticket: %s\n", input);
-		errno = temp;
 		return false;
 	}
 	node->fields = fields;
@@ -221,6 +181,23 @@ parsenearbyticket(const char *input, Node **tail, uintmax_t *tser)
 	return true;
 }
 
+static ParseState
+parsecase(const ParseState state,
+          char * const restrict input,
+          const char * const restrict nextstr,
+          bool (*parsefunc)(const char*),
+          const char * const restrict parsing)
+{
+	if (strcmp(input, nextstr) == 0)
+		return state + 1;
+	if (!parsefunc(input)) {
+		fprintf(stderr, "Could not parse %s: %s\n", parsing, input);
+		free(input);
+		exit(EXIT_FAILURE);
+	}
+	return state;
+}
+
 static uintmax_t
 parseinput(FILE * const in)
 {
@@ -228,48 +205,53 @@ parseinput(FILE * const in)
 	ParseState state = CONSTRAINTS;
 	uintmax_t tser = 0;
 	char *input;
-	int result;
-	while ((result = getline(in, &input)) == 1) {
+	while (getline(in, &input) == 1) {
 		switch (state) {
 		case CONSTRAINTS:
-			if (strcmp(input, "your ticket:") == 0) {
-				state = YOUR_TICKET;
-				break;
-			}
-			if (!parsedef(input)) {
-				PRINT_ERR("Could not parse field definition");
-				free(input);
-				exit(EXIT_FAILURE);
-			}
+			state = parsecase(state,
+			                  input,
+			                  "your ticket:",
+			                  parsedef,
+			                  "ticket field definition");
 			break;
 		case YOUR_TICKET:
-			if (strcmp(input, "nearby tickets:") == 0) {
-				state = NEARBY_TICKETS;
-				break;
-			}
-			if (!parseyourticket(input)) {
-				PRINT_ERR("Could not parse your ticket");
-				free(input);
-				exit(EXIT_FAILURE);
-			}
+			state = parsecase(state,
+			                  input,
+			                  "nearby tickets:",
+			                  parseyourticket,
+			                  "your ticket");
 			break;
 		case NEARBY_TICKETS:
 			if (!parsenearbyticket(input, &tail, &tser)) {
-				PRINT_ERR("Could not parse nearby ticket");
+				fprintf(stderr,
+				        "Could not parse nearby ticket: %s\n",
+				        input);
 				free(input);
 				exit(EXIT_FAILURE);
 			}
 		}
 		free(input);
 	}
-	if (ferror(stdin)) {
-		PRINT_ERR("Input failed");
-		exit(EXIT_FAILURE);
-	} else if (result != EOF) {
-		fputs("Bad input format\n", stderr);
+	if (!feof(in) || ferror(in)) {
+		fputs("Error occured while parsing puzzle input\n", stderr);
 		exit(EXIT_FAILURE);
 	}
 	return tser;
+}
+
+static void
+eliminateimplausible(const uintmax_t tikfields[const restrict nconstr],
+                     const bool fixed[const restrict nconstr],
+                     bool plausible[nconstr][nconstr])
+{
+	for (size_t i = 0; i < nconstr; i++) {
+		if (fixed[i])
+			continue;
+		for (size_t j = 0; j < nconstr; j++) {
+			if (!fixed[j] && !satisfies(tikfields[i], constr + j))
+				plausible[i][j] = false;
+		}
+	}
 }
 
 static void
@@ -284,16 +266,8 @@ sortfields(void)
 			for (size_t j = 0; j < nconstr; j++)
 				plausible[i][j] = !fixed[i] && !fixed[j];
 		}
-		for (const Node *t = tickets; t != NULL; t = t->next) {
-			for (size_t i = 0; i < nconstr; i++) {
-				if (fixed[i])
-					continue;
-				for (size_t j = 0; j < nconstr; j++) {
-					if (!fixed[j] && !satisfies(t->fields[i], constr[j]))
-						plausible[i][j] = false;
-				}
-			}
-		}
+		for (const Node *tik = tickets; tik != NULL; tik = tik->next)
+			eliminateimplausible(tik->fields, fixed, plausible);
 		for (size_t i = 0; i < nconstr; i++) {
 			if (fixed[i])
 				continue;
@@ -335,7 +309,7 @@ proddepart(void)
 	for (size_t i = 0; i < nconstr; i++) {
 		if (strncmp(constr[i].name, "departure", 9) == 0) {
 			if (p >= UINTMAX_MAX / yourticket[i]) {
-				fputs("Integer overflow detected\n", stderr);
+				fputs("Integer wraparound detected\n", stderr);
 				exit(EXIT_FAILURE);
 			}
 			p *= yourticket[i];
@@ -347,17 +321,15 @@ proddepart(void)
 static void
 freedata(void)
 {
-	regfree(&defreg);
 	for (size_t i = 0; i < nconstr; i++)
 		free(constr[i].name);
 	free(constr);
 	free(yourticket);
-	Node *node = tickets;
-	while (node != NULL) {
-		Node *const temp = node->next;
-		free(node->fields);
-		free(node);
-		node = temp;
+	while (tickets != NULL) {
+		Node * const next = tickets->next;
+		free(tickets->fields);
+		free(tickets);
+		tickets = next;
 	}
 }
 
@@ -366,18 +338,8 @@ day16(FILE * const in)
 {
 	if (atexit(freedata) != 0)
 		fputs("Call to `atexit` failed; memory may leak\n", stderr);
-	int result = regcomp(&defreg,
-	                     "^([A-Za-z ]+): " NUM_REG "-" NUM_REG " or "
-	                                       NUM_REG "-" NUM_REG "$",
-	                     REG_EXTENDED);
-	if (result != 0) {
-		char buf[128];
-		regerror(result, &defreg, buf, 128);
-		fprintf(stderr, "Could not compile regex: %s\n", buf);
-		return EXIT_FAILURE;
-	}
-	printf("TSER\t%" PRIuMAX "\n", parseinput(in));
+	printf("TSER\t%ju\n", parseinput(in));
 	sortfields();
-	printf("Depart\t%" PRIuMAX "\n", proddepart());
+	printf("Depart\t%ju\n", proddepart());
 	return EXIT_SUCCESS;
 }
--- 17.c
+++ 17.c
@@ -5,20 +5,10 @@
  * To Public License, Version 2, as published by Sam Hocevar. See
  * http://www.wtfpl.net/ for more details.
  */
-#include <errno.h>
-#include <inttypes.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define PRINT_ERR(s) \
-	do { \
-		if (errno != 0) \
-			perror(s); \
-		else \
-			fputs(s "\n", stderr); \
-	} while (false);
-
 
 #define ACTIVE_C(a, x, y, z, w, xs, ys, zs) \
 	a[(w) * (xs) * (ys) * (zs) + (z) * (xs) * (ys) + (y) * (xs) + (x)]
@@ -26,25 +16,21 @@
 #define ACTIVE(a, x, y, z, w) ACTIVE_C(a, x, y, z, w, xsize, ysize, zsize)
 
 static bool *pattern = NULL;
-static size_t width = 0;
-static size_t height = 0;
+static size_t width = 0, height = 0;
 
 static bool *space = NULL;
-static size_t xsize = 0;
-static size_t ysize = 0;
-static size_t zsize = 0;
-static size_t wsize = 0;
+static size_t xsize = 0, ysize = 0, zsize = 0, wsize = 0;
 
 /* During the parsing phase, `xsize` keeps track of the buffer size */
 static void
-bufappend(size_t x, bool val)
+bufappend(const size_t x, const bool val)
 {
 	const size_t i = height * width + x;
 	if (i >= xsize) {
 		xsize = (xsize > 0)? 2 * xsize : 1;
-		bool *const temp = realloc(pattern, xsize * sizeof(bool));
+		bool * const temp = realloc(pattern, xsize * sizeof(bool));
 		if (temp == NULL) {
-			PRINT_ERR("Allocation failed");
+			fputs("Could not allocate new pattern\n", stderr);
 			exit(EXIT_FAILURE);
 		}
 		pattern = temp;
@@ -88,8 +74,8 @@ parseinput(FILE * const in)
 			exit(EXIT_FAILURE);
 		}
 	}
-	if (ferror(in)) {
-		PRINT_ERR("Input parsing failed");
+	if (!feof(in) || ferror(in)) {
+		fputs("Error occured while parsing puzzle input\n", stderr);
 		exit(EXIT_FAILURE);
 	}
 	if (width > 0 && x == width) {
@@ -103,26 +89,23 @@ parseinput(FILE * const in)
 static void
 copypattern(void)
 {
-	errno = 0;
-	bool *const new = malloc(width * height * sizeof(bool));
+	bool * const new = malloc(width * height * sizeof(bool));
 	if (new == NULL) {
-		PRINT_ERR("Allocation failed");
+		fputs("Could not allocate pattern copy\n", stderr);
 		exit(EXIT_FAILURE);
 	}
 	xsize = width;
 	ysize = height;
-	zsize = 1;
-	wsize = 1;
+	zsize = wsize = 1;
 	for (size_t y = 0; y < height; y++) {
-		for (size_t x = 0; x < width; x++) {
+		for (size_t x = 0; x < width; x++)
 			ACTIVE(new, x, y, 0, 0) = pattern[y * width + x];
-		}
 	}
 	space = new;
 }
 
 static bool
-isborder(size_t x, size_t y, size_t z, size_t w)
+isborder(const size_t x, const size_t y, const size_t z, const size_t w)
 {
 	return x == 0 || x == xsize - 1 ||
 	       y == 0 || y == ysize - 1 ||
@@ -147,24 +130,22 @@ scanborders(bool *incx, bool *incy, bool *incz, bool *incw)
 			*incz = true;
 		if (w == 0 || w == wsize - 1)
 			*incw = true;
-		stop = *incx && *incy
-		       && *incz && *incw;
+		stop = *incx && *incy && *incz && *incw;
 	}
 }
 
 #define INNER(v) size_t v = 1; v + 1 < new##v; v++
 
 static void
-resizespace(bool incx, bool incy, bool incz, bool incw)
+resizespace(const bool incx, const bool incy, const bool incz, const bool incw)
 {
-	size_t newx = incx? xsize + 2 : xsize;
-	size_t newy = incy? ysize + 2 : ysize;
-	size_t newz = incz? zsize + 2 : zsize;
-	size_t neww = incw? wsize + 2 : wsize;
-	errno = 0;
+	const size_t newx = xsize + 2 * incx;
+	const size_t newy = ysize + 2 * incy;
+	const size_t newz = zsize + 2 * incz;
+	const size_t neww = wsize + 2 * incw;
 	bool *const new = calloc(newx * newy * newz * neww, sizeof(bool));
 	if (new == NULL) {
-		PRINT_ERR("Allocation failed");
+		fputs("Could not reallocate space\n", stderr);
 		exit(EXIT_FAILURE);
 	}
 	for (INNER(w)) for (INNER(z)) for (INNER(y)) for (INNER(x)) {
@@ -187,8 +168,9 @@ countneighbors(size_t xx, size_t yy, size_t zz, size_t ww)
 {
 	uint_fast8_t total = 0;
 	for (NEIGH(w)) for (NEIGH(z)) for (NEIGH(y)) for (NEIGH(x)) {
-		total += !(x == xx && y == yy && z == zz && w == ww)
-		         && ACTIVE(space, x, y, z, w);
+		if (!(x == xx && y == yy && z == zz && w == ww)
+		    && ACTIVE(space, x, y, z, w))
+			total++;
 	}
 	return total;
 }
@@ -209,10 +191,9 @@ runcycle(void)
 	scanborders(&incx, &incy, &incz, &incw);
 	if (incx || incy || incz || incw)
 		resizespace(incx, incy, incz, incw);
-	errno = 0;
-	bool *const new = malloc(xsize * ysize * zsize * wsize * sizeof(bool));
+	bool * const new = malloc(xsize * ysize * zsize * wsize * sizeof(bool));
 	if (new == NULL) {
-		PRINT_ERR("Allocation failed");
+		fputs("Could not allocate new space\n", stderr);
 		exit(EXIT_FAILURE);
 	}
 	for (size_t w = 0; w < wsize; w++) {
@@ -276,12 +257,12 @@ day17(FILE * const in)
 		runcycle();
 		cleanw();
 	}
-	printf("3D\t%" PRIuMAX "\n", countactive());
+	printf("3D\t%ju\n", countactive());
 	free(space);
 	space = NULL;
 	copypattern();
 	for (uint_fast8_t cycle = 1; cycle <= 6; cycle++)
 		runcycle();
-	printf("4D\t%" PRIuMAX "\n", countactive());
+	printf("4D\t%ju\n", countactive());
 	return EXIT_SUCCESS;
 }
